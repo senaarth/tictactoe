2592efbc0b431ecdb292baa24a2585cc28e38f8d
Lucas Andrade <64823667+lucas-andrad@users.noreply.github.com>
2020-06-29 20:22:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
+// Declaring variables
 let boxes = document.querySelectorAll("td")
 let plays1 = 0;
 let plays2 = 0;
+let player1 = []
+let player2 = []
 
+// Adding click in each field
 for (i = 0; i < boxes.length; i++) {
     boxes[i].addEventListener("click", function() {
+        // Verifying if the field is filled
         let filled;
         if (this.innerText == "") {
             filled = false;
-            console.log("tem nada aqui mano")
         } else {
             filled = true;
         }
         if (!filled) {
 
-
+            // filling the field after verifying which turn is
             if (plays1 == plays2) {
                 this.innerText = "X";
                 plays1++;
@@ -22,6 +26,10 @@ for (i = 0; i < boxes.length; i++) {
                 plays2++;
             }
         }
-
+        checkWin(player1)
     })
 }
+
+
+// function for check winner
+function checkWin(){}
